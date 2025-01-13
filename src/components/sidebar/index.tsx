5faab2436be0eba8bd6325b68cdb192ac91d084c
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2025-01-13 02:32:09 -0300

    Projeto-dashboard-next-shadcn-ui

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import { Button } from "../ui/button";
 import { Sheet, SheetTrigger, SheetContent } from "../ui/sheet";
-import { PanelBottom } from "lucide-react";
+import { Home, Package, PanelBottom, Settings, ShoppingBag, Users } from "lucide-react";
 
 export function Sidebar(){
   return(
@@ -13,6 +13,7 @@ export function Sidebar(){
           sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6"
         >
           <Sheet>
+
             <SheetTrigger asChild>
               <Button size={'icon'} variant={'outline'} className="sm:hidden">
                 <PanelBottom className="w-5 h-5"/>
@@ -20,17 +21,70 @@ export function Sidebar(){
               </Button>
             </SheetTrigger>
 
-            <SheetContent>
-              <nav>
-                <Link href={'#'}>
-                  <span>Logo</span>
+            <SheetContent side={'right'} className="sm:max-w-xl">
+              <nav className="grid gap-6 text-lg font-medium">
+                <Link 
+                  href={'#'}
+                  className="flex h-10 w-10 bg-primary rounded-full text-lg 
+                  items-center justify-center text-primary-foreground md:text-base gap-2"
+                  prefetch={false}
+                >
+                  <Package className="w-5 h-5 transition-all"/> 
+                  <span className="sr-only">Logo do projeto</span>
                 </Link>
+
+                <Link
+                  href={'#'}
+                  className="flex items-center gap-4 px-2 text-muted-foreground hover:text-foreground"
+                  prefetch={false}
+                >
+                  <Home className="w-5 h-5 transition-all" />
+                  Início
+                </Link>
+
+                <Link
+                  href={'#'}
+                  className="flex items-center gap-4 px-2 text-muted-foreground hover:text-foreground"
+                  prefetch={false}
+                >
+                  <ShoppingBag className="w-5 h-5 transition-all" />
+                  Pedidos
+                </Link>
+
+                <Link
+                  href={'#'}
+                  className="flex items-center gap-4 px-2 text-muted-foreground hover:text-foreground"
+                  prefetch={false}
+                >
+                  <Package className="w-5 h-5 transition-all" />
+                  Produtos
+                </Link>
+
+                <Link
+                  href={'#'}
+                  className="flex items-center gap-4 px-2 text-muted-foreground hover:text-foreground"
+                  prefetch={false}
+                >
+                  <Users className="w-5 h-5 transition-all" />
+                  Clientes
+                </Link>
+
+                <Link
+                  href={'#'}
+                  className="flex items-center gap-4 px-2 text-muted-foreground hover:text-foreground"
+                  prefetch={false}
+                >
+                  <Settings className="w-5 h-5 transition-all" />
+                  Configurações
+                </Link>
+
               </nav>
             </SheetContent>
           </Sheet>
+          <h2>Menu</h2>
         </header>
       </div>
-      
+
     </div>
   )
 }
