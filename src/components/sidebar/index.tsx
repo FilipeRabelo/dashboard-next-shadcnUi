4b3f3b0adecf77d0a85047ca9cd3dd5b24e3c656
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2025-01-13 02:14:19 -0300

    Projeto-dashboard-next-shadcn-ui

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,16 +1,22 @@
 import Link from "next/link";
 import { Button } from "../ui/button";
 import { Sheet, SheetTrigger, SheetContent } from "../ui/sheet";
+import { PanelBottom } from "lucide-react";
 
 export function Sidebar(){
   return(
     <div className="flex w-full flex-col bg-muted/40">
-      <div className="flex flex-col">
-        <header>
+
+      <div className="sm:hidden flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
+        <header 
+          className="sticky top-0 z-30 flex h-14 items-center px-4 border-b bg-background gap-4
+          sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6"
+        >
           <Sheet>
             <SheetTrigger asChild>
-              <Button>
-                <span>Abrir</span>
+              <Button size={'icon'} variant={'outline'} className="sm:hidden">
+                <PanelBottom className="w-5 h-5"/>
+                <span className="sr-only">Abrir / Fechar Menu</span>
               </Button>
             </SheetTrigger>
 
@@ -24,6 +30,7 @@ export function Sidebar(){
           </Sheet>
         </header>
       </div>
+      
     </div>
   )
 }
